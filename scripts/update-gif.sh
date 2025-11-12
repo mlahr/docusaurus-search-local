a59980c23de008b04c8f78e02f2c7645d236cf59
Michael Lahr <michael.lahr@gmail.com>
2025-11-12 17:09:50 +0800

    chore: remove unused files and update license

--- scripts/update-gif.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ev
-node --unhandled-rejections=strict scripts/create-gif.js
-ffmpeg -y -ss 3 -i scripts/preview.webm -vf "fps=10,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 docs/preview.gif
-rm scripts/preview.webm
